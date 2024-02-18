4124999536e6a0893045ef256db62d3080227e62
Michael Scarr <michael.m.scarr@gmail.com>
2024-02-18 15:25:39 -0600

    test

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ export default {
         "dialect": "mysql"
     },
     "test": {
-        "username": "",
+        "username": "0",
         "password": "",
         "database": "",
         "port": "3306",
