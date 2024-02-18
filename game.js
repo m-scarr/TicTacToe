d5fbfde0c3a845e2fb1a69ced1b1a8799fe67d23
Michael Scarr <michael.m.scarr@gmail.com>
2024-02-18 14:53:03 -0600

    small changes to data structure

--- game.js
+++ game.js
@@ -35,13 +35,33 @@ class Game {
         const newGame = new Game();
         newGame.roomName = gameData.roomName;
         newGame.turn = gameData.turn;
-        newGame.grid = gameData.grid;//
+        newGame.grid = gameData.grid;
         return newGame;
     }
 
     placeSymbol(x, y) {
         this.grid[x][y] = this.turn;
         this.turn = (this.turn === "X" ? "O" : "X");
+    }
+
+    checkForVictor() {
+        for (let i = 0; i < 1; i++) {
+            const symbol = (i === 0 ? "X" : "O");
+            for (let x = 0; x < 2; x++) {
+                if (this.grid[x][0] === symbol && this.grid[x][1] === symbol && this.grid[x][2] === symbol) {
+                    return symbol;
+                }
+            }
+            for (let y = 0; y < 2; y++) {
+                if (this.grid[0][y] === symbol && this.grid[1][y] === symbol && this.grid[2][y] === symbol) {
+                    return symbol;
+                }
+            }
+            if (this.grid[1][1] === symbol && ((this.grid[0][0] === symbol && this.grid[2][2] === symbol) || (this.grid[2][0] === symbol && this.grid[0][2] === symbol))) {
+                return symbol;
+            }
+        }
+        return null;
     }
 
     toString() {
