cb43e388bac74006b60dcb8edf518c2c918bbf3f
Michael Scarr <michael.m.scarr@gmail.com>
2024-02-18 15:03:33 -0600

    small changes to the game data structure

--- game.js
+++ game.js
@@ -15,27 +15,36 @@ function parseJSONRecursively(jsonString) {
 
 
 class Game {
-    roomName;
     turn;
     grid = [
         [null, null, null],
         [null, null, null],
         [null, null, null],
     ];
+    players = {
+        X: null,
+        Y: null
+    }
 
-    static create(roomName) {
+    static create(player1Id, player2Id) {
         const newGame = new Game();
-        newGame.roomName = roomName;
         newGame.turn = Math.random() < .5 ? "X" : "O";
+        if (Math.random < .5) {
+            newGame.players.X = player1Id;
+            newGame.players.Y = player2Id;
+        } else {
+            newGame.players.X = player2Id;
+            newGame.players.Y = player1Id;
+        }
         return newGame;
     }
 
     static parse(gameString) {
         const gameData = parseJSONRecursively(gameString);
         const newGame = new Game();
-        newGame.roomName = gameData.roomName;
         newGame.turn = gameData.turn;
         newGame.grid = gameData.grid;
+        newGame.players = gameData.players;
         return newGame;
     }
 
@@ -49,22 +58,22 @@ class Game {
             const symbol = (i === 0 ? "X" : "O");
             for (let x = 0; x < 2; x++) {
                 if (this.grid[x][0] === symbol && this.grid[x][1] === symbol && this.grid[x][2] === symbol) {
-                    return symbol;
+                    return players[symbol];
                 }
             }
             for (let y = 0; y < 2; y++) {
                 if (this.grid[0][y] === symbol && this.grid[1][y] === symbol && this.grid[2][y] === symbol) {
-                    return symbol;
+                    return players[symbol];
                 }
             }
             if (this.grid[1][1] === symbol && ((this.grid[0][0] === symbol && this.grid[2][2] === symbol) || (this.grid[2][0] === symbol && this.grid[0][2] === symbol))) {
-                return symbol;
+                return players[symbol];
             }
         }
         return null;
     }
 
     toString() {
-        return JSON.stringify({ grid: this.grid, turn: this.turn, roomName: this.roomName });
+        return JSON.stringify({ grid: this.grid, turn: this.turn, players: this.players });
     }
 }
