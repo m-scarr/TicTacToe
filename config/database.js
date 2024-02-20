2ce8b609307d3cafd774cca1bfef89d6a86fd84f
BuildTools <unconfigured@null.spigotmc.org>
2024-02-20 08:31:11 -0500

    Redis Integration phase 1

--- config/database.js
+++ config/database.js
@@ -6,21 +6,5 @@ export default {
         "port": "3306",
         "host": "127.0.0.1",
         "dialect": "mysql"
-    },
-    "test": {
-        "username": "",
-        "password": "",
-        "database": "",
-        "port": "3306",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
-    },
-    "production": {
-        "database": "",
-        "username": "",
-        "password": "",
-        "port": "3306",
-        "host": "",
-        "dialect": "mysql"
     }
 }
