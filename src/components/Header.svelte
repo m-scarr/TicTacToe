202c2d6d4908d1d33879c3901fe82ebabfb5f747
Michael Scarr <michael.m.scarr@gmail.com>
2024-02-23 16:43:06 -0600

    did some styling to the log in and register pages

---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+<div class="app-header">
+    Tic Tac Toe
+</div>
+<style>
+    .app-header {
+        width: 100%;
+        color: white;
+        margin-bottom: 8px;
+        font-size: 32px;
+    }
+</style>
--- src/views/LogIn.svelte
+++ src/views/LogIn.svelte
@@ -8,21 +8,45 @@
 </script>
 
 <main>
-    Tic Tac Toe
-    <hr />
-    <input type="text" bind:value={username} /><br />
-    <input type="text" bind:value={password} /><br />
-    <button
-        on:click={() => {
-            API.user.logIn(username, password);
-        }}>Log In</button
-    >
-    <button
-        on:click={() => {
-            currentViewStore.set(View.Register);
-        }}>Create an Account</button
-    >
+    <div class="log-in-container">
+        <i>Welcome Back!</i><br/>
+        <input type="text" bind:value={username} />
+        <input type="password" bind:value={password} />
+        <hr />
+        <button
+            class="log-in-button"
+            on:click={() => {
+                API.user.logIn(username, password);
+            }}>Log In</button
+        >
+        <button
+            class="create-account-button"
+            on:click={() => {
+                currentViewStore.set(View.Register);
+            }}>Create an Account</button
+        >
+    </div>
 </main>
 
 <style>
+    .log-in-container {
+        display: flex;
+        flex-direction: column;
+        padding: 16px;
+    }
+    .log-in-container button {
+        margin: 8px auto 8px auto;
+        width: 60%;
+        min-width: 320px;
+    }
+    .log-in-container input {
+        margin: 8px auto 8px auto;
+        width: 60%;
+        min-width: 320px;
+    }
+    .log-in-container hr {
+        margin: 8px auto 8px auto;
+        width: 60%;
+        min-width: 320px;
+    }
 </style>
